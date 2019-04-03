ec3587cbe69cf7ec5576748efdc0bbf1e2555c50
AterCatus <myleelee2000@nate.com>
2019-04-03 15:25:08 +0900

    Implement sample code of motor controll

---
+++ motor.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as gpio
+import time
+
+def init():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT)
+    gpio.setup(22, gpio.OUT)
+    gpio.setup(23, gpio.OUT)
+    gpio.setup(24, gpio.OUT)
+
+def forward():
+    init()
+    gpio.output(17, False)
+    gpio.output(22, True)
+    gpio.output(23, False)
+    gpio.output(24, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def reverse(tf):
+    init()
+    gpio.output(17, True)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    gpio.output(24, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+print("forward")
+forward(4)
+print("backward")
+reverse(2)
