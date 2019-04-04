4affd34a289ebc9343d3a2f7a8fd214bc8c8140e
Atercatus <myleelee2053@naver.com>
2019-04-04 11:55:22 +0100

    Implement basic DC motor control source code

--- motor.py
+++ motor.py
@@ -1,32 +1,24 @@
 import RPi.GPIO as gpio
 import time
 
-def init():
-    gpio.setmode(gpio.BCM)
-    gpio.setup(17, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
-    gpio.setup(23, gpio.OUT)
-    gpio.setup(24, gpio.OUT)
+IN1 = 7
+IN2 = 11
+IN3 = 13
+IN4 = 15
+SERVO = 12
 
-def forward():
-    init()
-    gpio.output(17, False)
-    gpio.output(22, True)
-    gpio.output(23, False)
-    gpio.output(24, True)
-    time.sleep(tf)
-    gpio.cleanup()
+gpio.setmode(gpio.BOARD)
 
-def reverse(tf):
-    init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
-    time.sleep(tf)
-    gpio.cleanup()
+gpio.setup(IN1, gpio.OUT)
+gpio.setup(IN2, gpio.OUT)
+gpio.setup(IN3, gpio.OUT)
+gpio.setup(IN4, gpio.OUT)
 
-print("forward")
-forward(4)
-print("backward")
-reverse(2)
+gpio.output(IN1, False)
+gpio.output(IN2,True)
+gpio.output(IN3, True)
+gpio.output(IN4, True)
+
+time.sleep(2)
+
+gpio.cleanup()
