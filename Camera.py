5e096d4d78bdd96ac036951acb9d738c0ceb7495
Atercatus <myleelee2053@naver.com>
2019-04-16 09:42:29 +0100

    test

--- Camera.py
+++ Camera.py
@@ -3,15 +3,15 @@ import os
 import numpy as np
 
 class Camera:
-    path = "C:\\공부자료\\DataSet" #경로지정
+    path = "" #경로지정
     imageList = [None,None,None] # 이미지 리스트
     left_video_capture = None # 왼쪽 카메라
     middle_video_capture = None # 중간 카메라
     right_video_capture = None # 오른쪽 카메라
     def __init__(self):#생성자
-        self.left_video_capture = cv2.VideoCapture(1) #1번연결
-        self.middle_video_capture = cv2.VideoCapture(2) #2번연결
-        self.right_video_capture = cv2.VideoCapture(3) #3번연결
+        self.left_video_capture = cv2.VideoCapture(0) #1번연결
+        self.middle_video_capture = cv2.VideoCapture(1) #2번연결
+        self.right_video_capture = cv2.VideoCapture(2) #3번연결
     def get_image(self):
         ret1 , frame1 = self.left_video_capture.read() #순간사진찍기
         ret2 , frame2 = self.middle_video_capture.read() #순간사진찍기
@@ -34,8 +34,8 @@ class Camera:
 controlCamera = Camera()
 controlCamera.record()
 imageList = controlCamera.get_image()
-cv2.imshow("leftCamera",imageList[0])
-cv2.imshow("middleCamera",imageList[1])
-cv2.imshow("rightCamera",imageList[2])
+print("leftCamera",imageList[0])
+print("middleCamera",imageList[1])
+print("rightCamera",imageList[2])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
